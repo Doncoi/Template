119d7f30ccc6871d31d57d04e7e6d833ef564e6b
Doncoi <eiche2016@outlook.com>
2019-12-03 15:26:41 +0800

    12/03/2019

--- Confirmed/quicksort.cpp
+++ Confirmed/quicksort.cpp
@@ -1,5 +1,26 @@
 void quick_sort(int l, int r)
 {
+	if(l >= r) return;
+	int i = l, j = r, k = a[(l + r) / 2];
+
+	do
+	{
+		while(a[i] < k) ++ i;
+		while(a[j] > k) -- j;
+		if(i <= j) 
+		{
+			std :: swap(a[i ++], a[j --]);
+		}
+	} while(i <= j);
+
+	if(l < j) quick_sort(l, j);
+	if(i < r) quick_sort(i, r);
+}
+
+//未确认的写法
+/*
+void quick_sort(int l, int r)
+{
 	if(l > r) return;
 	int i, j, t, tmp;
 
@@ -19,4 +40,4 @@ void quick_sort(int l, int r)
 
 	quick_sort(l, i - 1);
 	quick_sort(i + 1, r);
-}
+}*/
--- Confirmed/快读.cpp
+++ Confirmed/快读.cpp
@@ -4,3 +4,11 @@ inline int read() {
 	while(ch <= '9' && ch >= '0') {res = res*10 + ch-'0'; ch = getchar();}
 	return (res * flag);
 }
+
+template<typename Temp> inline void read(Temp &x)
+{
+	Temp res = 0, flg = 1; char chr = getchar();
+	while(! isdigit(chr)) {if(chr == '-') flg = -1; chr = getchar();} 
+	while(isdigit(chr)) {res = res * 10 + chr - '0'; chr = getchar();}
+	x = (res * flg);
+}
